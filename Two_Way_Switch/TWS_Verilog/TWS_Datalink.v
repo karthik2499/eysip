db7862e8eb11c49721c5702d3595cf3140ea06c3
karthik2499 <kshet2499@gmail.com>
2020-05-27 00:18:00 +0530

    first

--- Two_Way_Switch/TWS_Verilog/TWS_Datalink.v
+++
@@ -1,5 +0,0 @@
-module TWS_Datalink(
-	input s1,s2,		//Define two Inputs pins
-	output X1);		//Define one output pin
-	assign X1 = (s1^s2);	//XOR the 2 input pins(s1,s2) and assign the output to the output pin(out)
-endmodule
